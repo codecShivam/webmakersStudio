139ae56e652356d5a79173e00803d411ca9b7afc
codecShivam <codecshivam@gmail.com>
2023-12-23 03:13:00 +0530

    build company section with logos

---
+++ src/components/Companies.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Zapier from "../assets/companies/zapier.svg";
+import Spotify from "../assets/companies/spotify.svg";
+import Amazon from "../assets/companies/amazon.svg";
+import Zoom from "../assets/companies/zoom.svg";
+import Slack from "../assets/companies/slack.svg";
+import Adobe from "../assets/companies/adobe.svg";
+import Marquee from "react-fast-marquee";
+
+const Companies = () => {
+  const logos = [Zapier, Spotify, Amazon, Zoom, Slack, Adobe];
+
+  return (
+    <div className="flex flex-col justify-center items-center mt-16">
+      <h1 className="text-xl tracking-tight font-light font-serif text-center text-gray-500 pb-4">
+        Trusted By 250+ Companies
+      </h1>
+      <Marquee>
+        {logos.map((logo, index) => (
+          <img key={index} src={logo} alt="" className="w-60 mx-4" />
+        ))}
+      </Marquee>
+    </div>
+  );
+};
+
+export default Companies;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,15 +2,20 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <div className="flex flex-col justify-end pb-12 items-center space-y-8 h-[60vh] p-4">
-      <h1 className="text-7xl text-center font-semibold text-gray-800 pb-12">
-        Elevate Your Presence with <br></br> Seamless Design and Innovation.
+    <div className="flex flex-col justify-end pb-12 items-center space-y-6 sm:h-[58vh] p-4">
+      {/* Heading */}
+      <h1 className="text-5xl text-center font-semibold text-gray-800  sm:text-7xl">
+        Elevate Your Presence with <br /> Seamless Design and Innovation.
       </h1>
-      <p className="text-xl font-light text-gray-600">
+
+      {/* Paragraph */}
+      <p className="text-xl font-light text-gray-600 sm:text-lg pb-12">
         From strategic planning to digital presence, we're your dedicated
         partner in achieving solo success.
       </p>
-      <button className="text-gray-50 border bg-gray-700 border-gray-500 py-2  mt-16 px-6 rounded-md">
+
+      {/* Button */}
+      <button className="text-gray-50 border bg-gray-700 border-gray-500 py-2 mt-8 px-6 rounded-md">
         View Pricing
       </button>
     </div>
