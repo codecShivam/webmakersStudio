827e3aa1a7063834a63c3379b49b70c3e731df54
codecShivam <codecshivam@gmail.com>
2023-12-24 06:42:47 +0530

    fix animation effect

--- src/components/AnimatedImage.jsx
+++ src/components/AnimatedImage.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import { useSpring, animated, config } from "react-spring";
 import { useInView } from "react-intersection-observer";
 
-const AnimatedImage = ({ src, index }) => {
+const AnimatedImage = ({ src }) => {
   const [ref, inView] = useInView({
     triggerOnce: false,
-    threshold: 0.5,
+    threshold: 0.4,
   });
 
   const props = useSpring({
@@ -14,15 +14,12 @@ const AnimatedImage = ({ src, index }) => {
     config: config.wobbly,
   });
 
-
-
-
   return (
     <animated.img
       ref={ref}
       src={src}
       alt=""
-      className={`w-80 mx-4 my-4 rounded-md hover:shadow-2xl hover:shadow-gray-700 hover:cursor-pointer`}
+      className={`w-96 mx-4 my-4 rounded-md hover:shadow-2xl hover:shadow-gray-700 hover:cursor-pointer`}
       style={props}
     />
   );
